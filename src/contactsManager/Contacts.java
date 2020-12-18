8805bd27b9ff7ce9d64694bc7063d28a1f27c545
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-18 16:31:07 -0600

    adding contactsManager Directory with Contacts and ContactsManagerApplication classes

--- src/contactsManager/Contacts.java
+++ src/contactsManager/Contacts.java
@@ -1,4 +1,24 @@
 package contactsManager;
 
 public class Contacts {
+    //properties
+    private String firstName;
+    private String lastName;
+    private Long phoneNumber;
+
+
+    //Contacts Constructor
+    public Contacts(String firstName, String lastName, Long phoneNumber) {
+
+    }
+
+
+    //Custom Methods
+
+    //loadContacts method used to display contacts
+    public void loadContacts(){
+
+    }
+
+
 }
---
+++ src/contactsManager/ContactsManagerApplication.java
@@ -0,0 +1,9 @@
+package contactsManager;
+
+public class ContactsManagerApplication {
+
+    //Main Method
+    public static void main(String[] args) {
+
+    }
+}
