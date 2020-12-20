76b081c645106c6a9728ca1fcfdbcf30131cac8c
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-20 16:12:22 -0600

    add: ContactCRUD class with methods for functionality, update: Input class, Contacts class. test: Input methods

---
+++ src/contactsManager/ContactCRUD.java
@@ -0,0 +1,41 @@
+package contactsManager;
+
+public class ContactCRUD {
+
+    //Constructors
+    public ContactCRUD() {
+
+    }
+
+    //Custom Methods
+
+    //Method for loading contacts onto the user interface
+    public void loadContacts() {
+
+    }
+
+    //Method for displaying the main menu with options for the user to choose from
+    public void displayMainMenu() {
+
+    }
+
+    //Method for searching a contact by name
+    public void searchContactByName() {
+
+    }
+
+    //Method for updating contact list based on user's choice from the displayMainMenu
+    public void updateContactList() {
+
+    }
+
+    //Method for adding a contact
+    public void addContact() {
+
+    }
+
+    //Method for deleting a contact
+    public void deleteContact() {
+
+    }
+}
