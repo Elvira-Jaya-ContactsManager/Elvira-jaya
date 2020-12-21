8ca6a1a3550cdb5ad457f607a3a91eb09b2d0887
jaya <jayaseyyadri@gmail.com>
2020-12-21 16:32:31 -0600

    worked on reading from contactsReader

--- src/contactsManager/ContactsManagerApplication.java
+++ src/contactsManager/ContactsManagerApplication.java
@@ -12,7 +12,6 @@ public class ContactsManagerApplication {
 
 
 //        long phoneNumber = input.getPhoneNumber(); //storing the radius from the user input
-
 //        String firstName = input.getName("Enter your first name:"); //creating a new circle using radius1
 //        String lastName = input.getName("Enter your last name:"); //creating a new circle using radius1
 //
@@ -20,15 +19,8 @@ public class ContactsManagerApplication {
 //        System.out.println(lastName);
 //        System.out.println(phoneNumber);
 
-//        ContactCRUD crudExample = new ContactCRUD();
-//
-//        crudExample.displayMainMenu();
-
-        // Set up a new instance to access the jolts.txt file
+        ContactCRUD crudExample = new ContactCRUD();
+        crudExample.loadContacts();
         FileReader contactsReader = new FileReader("src/contactsManager", "contacts.txt", "contacts.log");
-        contactsReader.writeToLog("Successfully read the " + contactsReader.getFileName() + " file!"); // => src/contactsManager/contacts.txt
-//        System.out.println("Contacts file, here's the first line:");
-
-
     }
 }
