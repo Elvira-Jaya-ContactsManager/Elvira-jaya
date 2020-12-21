9a68dc50325cbbaab1635482def58fe9d5e2e257
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-21 17:02:40 -0600

    test: updateContactlist method and addContact method

--- src/contactsManager/ContactCRUD.java
+++ src/contactsManager/ContactCRUD.java
@@ -11,7 +11,6 @@ import java.util.Scanner;
 public class ContactCRUD extends Input {
     Input userInput = new Input();
     FileReader contactsReader = new FileReader("src/contactsManager", "contacts.txt", "contacts.log");
-//    List<String> lines = Files.readAllLines('')
 
     //Constructors
     public ContactCRUD() throws IOException {
@@ -34,14 +33,24 @@ public class ContactCRUD extends Input {
         return userChoice;
     }
 
+//    //Method for updating contact list based on user's choice from the displayMainMenu
+//    public void updateContactList(int userChoice) throws IOException {
+//        switch(userChoice) {
+//            case 1:
+//                loadContacts();
+//                displayMainMenu();
+//                break;
+//            case 2:
+////                addContact();
+//        }
+//
+//    }
+
     //Method for loading contacts onto the user interface
     public void loadContacts() throws IOException {
-        //will need to load contacts from file(1.create contacts.txt, able to load ot from the file)
-        // Set up a new instance to access the jolts.txt file
-
-        contactsReader.writeToLog("Successfully read the " + contactsReader.getFileName() + " file!"); // => src/contactsManager/contacts.txt
-        System.out.printf(" Name  |  Phone number  \n -------------------\n");
-        for(int i = 0;i<contactsReader.getFileLines().size();i++) {
+        contactsReader.writeToLog("Successfully read the " + contactsReader.getFileName() + " file!");
+        System.out.printf("%-15s | %s  \n -----------------------------\n", "Name", "Phone Number");
+        for(int i = 0; i < contactsReader.getFileLines().size(); i++) {
             System.out.println(contactsReader.getFileLines().get(i));
         }
 
@@ -52,24 +61,22 @@ public class ContactCRUD extends Input {
 
     }
 
-    //Method for updating contact list based on user's choice from the displayMainMenu
-    public void updateContactList() {
 
-    }
-
-    //Method for adding a contact
-
+//    //Method for adding a contact
 //    public void addContact() {
 //        Input userInput = new Input();
 //        int userChoice = displayMainMenu();
 //
-//        if(userChoice == 2){
-//            userInput.getName("Enter a name:");
-//             working on User Input to get the next Int
-//            userInput.getPhoneNumber();
-//
+//        try {
+//            if(userChoice == 2){
+//                userInput.getName("Enter a name:");
+////                userInput.getPhoneNumber();
+//            }
+//        }
+//        catch(S) {
 //
 //        }
+//
 //    }
 
     //Method for deleting a contact
