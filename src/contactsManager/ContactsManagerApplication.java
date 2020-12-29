8f94fcf01370d2056b803ce1f85f48138ecd2245
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-29 17:15:43 -0600

    feat: delete contact method

--- src/contactsManager/ContactsManagerApplication.java
+++ src/contactsManager/ContactsManagerApplication.java
@@ -11,14 +11,3 @@ public class ContactsManagerApplication {
         contactsManager.updateContactList();
     }
 }
-
-//        BUFFER CODE:
-//        long phoneNumber = input.getPhoneNumber(); //storing the radius from the user input
-//        String firstName = input.getName("Enter your first name:"); //creating a new circle using radius1
-//        String lastName = input.getName("Enter your last name:"); //creating a new circle using radius1
-//
-//        System.out.println(firstName);
-//        System.out.println(lastName);
-//        System.out.println(phoneNumber);
-
-//        FileReader contactsReader = new FileReader("src/contactsManager", "contacts.txt", "contacts.log");
